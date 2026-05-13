3aeae72e328928c08c8dde6f6c9ab8ea7175ac77
Afnan49 <afnanmahmoud872@gmail.com>
2026-05-13 16:49:44 +0300

    deploy backend

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  BaseUrl: 'https://your-api-name.onrender.com', // Replace with your deployed API URL
+  BaseUrl:
+    'https://ed655ff0-ad2f-4913-929c-36a044a6e7bd-00-f4mlajzn69hy.kirk.replit.dev/',
   production: true,
 };
