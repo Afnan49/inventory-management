f8f903ed1d08c97032d43259385c85c75ed3a9fa
Afnan49 <afnanmahmoud872@gmail.com>
2026-05-18 01:10:52 +0300

    edit api url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   BaseUrl:
-    'https://ed655ff0-ad2f-4913-929c-36a044a6e7bd-00-f4mlajzn69hy.kirk.replit.dev/',
+    'https://6cbebc74-6abe-4c62-b1ba-ee942e594a65-00-1ewz9jtun2ugw.picard.replit.dev/',
   production: true,
   imgbbApiKey: '2e505d19713890ecabeee7c469ad9617',
-  //frontendUrl: 'https://inventory-application1.netlify.app/login',
+  //frontendUrl: 'https://inventory-application1.netlify.app',
 };
