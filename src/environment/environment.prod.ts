c0543856bb1f24107f02514947f30288ad6361b5
Afnan49 <afnanmahmoud872@gmail.com>
2026-09-07 16:43:26 +0300

    edit

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   BaseUrl: '/api',
   production: true,
   imgbbApiKey: '2e505d19713890ecabeee7c469ad9617',
-  //frontendUrl: 'https://inventory-application1.netlify.app',
 };
